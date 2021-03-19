5d7720cd793710d3659dc51d821e2d15453be894
Enes BAŞKAYA <67550478+enesbaskaya@users.noreply.github.com>
2021-03-19 23:42:10 +0300

    v0.0.2
    UI düzenlemesi yapıldı
    FutureBuilder hatası düzeltildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 void main() => runApp(MaterialApp(
       title: 'Hava Durumu',
@@ -29,10 +31,7 @@ class HomeState extends State<Home> {
         'https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=istanbul',
         headers: headers);
     var results = jsonDecode(response.body);
-    print(results['result']);
-    setState(() {
-      res = results['result'];
-    });
+    return await results;
   }
 
   @override
@@ -43,74 +42,133 @@ class HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: getBody(res));
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: FutureBuilder(
+        future: getWeather(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (!snapshot.hasData)
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(child: CircularProgressIndicator()),
+              ],
+            );
+          else {
+            var data = snapshot.data['result'];
+            return getBody(data);
+          }
+        },
+      ),
+    );
   }
 
-  getBody(var res) {
+  getBody(var data) {
+    var firstDay = data[0];
+    /*var secondDay = data[1];
+    var thirdDay = data[2];
+    var fourthDay = data[3];
+    var fifthDay = data[4];
+    var sixthDay = data[5];
+    var seventhDay = data[6];*/
+    DateTime now = DateTime.now();
     return Column(
       children: [
         Container(
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                    Colors.transparent.withOpacity(0.6), BlendMode.dstATop),
+                image: (now.hour > 19 || now.hour < 5)
+                    ? AssetImage("assets/image/gece.jpg")
+                    : AssetImage("assets/image/gunduz.jpg")),
+          ),
           height: MediaQuery.of(context).size.height / 4,
           width: MediaQuery.of(context).size.width,
-          color: Colors.red,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
-                padding: EdgeInsets.only(bottom: 10),
-                child: Text(
-                  'Istanbul Hava Durumu',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600),
-                ),
+              SizedBox(
+                height: 35,
               ),
-              Padding(
-                padding: EdgeInsets.only(top: 3),
-                child: Text(
-                  res[0]['day'].toString() != null
-                      ? res[0]['day'].toString()
-                      : '',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    firstDay['day'].toString() != null
+                        ? firstDay['day'].toString()
+                        : '',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    firstDay['date'].toString() != null
+                        ? firstDay['date'].toString()
+                        : '',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.only(top: 3),
-                child: Text(
-                  res[0]['date'].toString() != null
-                      ? res[0]['date'].toString()
-                      : '',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600),
-                ),
+              SizedBox(
+                height: 6,
               ),
-              Padding(
-                padding: EdgeInsets.only(top: 3),
-                child: Text(
-                  res[0]['description'].toString().toUpperCase() != null
-                      ? res[0]['description'].toString().toUpperCase()
-                      : '',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.network(
+                    firstDay['icon'],
+                    placeholderBuilder: (context) =>
+                        CircularProgressIndicator(),
+                    height: 21.0,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    firstDay['description'].toString().toUpperCase() != null
+                        ? firstDay['description'].toString().toUpperCase()
+                        : '',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
               ),
-              Text(
-                res[0]['degree'].toString() != null
-                    ? res[0]['degree'].toString() + "\u00B0"
-                    : "",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w600),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FaIcon(
+                    FontAwesomeIcons.thermometerHalf,
+                    size: 18,
+                    color: Colors.red[700],
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    firstDay['degree'].toString().split('.')[0] != null
+                        ? firstDay['degree'].toString().split('.')[0] + "\u00B0"
+                        : "",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
               ),
             ],
           ),
@@ -124,8 +182,8 @@ class HomeState extends State<Home> {
                   leading: FaIcon(FontAwesomeIcons.thermometerHalf),
                   title: Text('Gündüz Derece'),
                   trailing: Text(
-                    res[0]['degree'].toString() != null
-                        ? res[0]['degree'].toString() + "\u00B0"
+                    firstDay['degree'].toString().split('.')[0] != null
+                        ? firstDay['degree'].toString().split('.')[0] + "\u00B0"
                         : "",
                   ),
                 ),
@@ -143,8 +201,8 @@ class HomeState extends State<Home> {
                   ),
                   title: Text('Hava Durumu'),
                   trailing: Text(
-                    '' /*res[0]['degree'].toString() != null
-                    ? res[0]['degree'].toString() + "\u00B0"
+                    '' /*firstDay['degree'].toString() != null
+                    ? firstDay['degree'].toString() + "\u00B0"
                     : ""*/
                     ,
                     style: TextStyle(
